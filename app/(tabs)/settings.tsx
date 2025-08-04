8a9628f7e1d4829e370126016ccf19c126e2d1dc
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2025-08-04 18:24:41 +0300

    Refactor tab layout and remove unused components; add settings page

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function settings() {
+  return (
+    <View>
+      <Text>This is the settings page</Text>
+    </View>
+  )
+}
